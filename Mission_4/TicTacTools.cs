cb4c60cf7d44da97a2b00b162b355f7450944e7c
lizzysimmons1 <153117350+lizzysimmons1@users.noreply.github.com>
2025-01-29 14:10:59 -0700

    fix aesthetic of board

--- Mission_4/TicTacTools.cs
+++ Mission_4/TicTacTools.cs
@@ -10,9 +10,9 @@ public class TicTacTools
         Console.WriteLine();
 
         Console.WriteLine(" {0} | {1} | {2} ", board[0], board[1], board[2]);
-        Console.WriteLine("---+---+---");
+        Console.WriteLine("--+--+--");
         Console.WriteLine(" {0} | {1} | {2} ", board[3], board[4], board[5]);
-        Console.WriteLine("---+---+---");
+        Console.WriteLine("--+--+--");
         Console.WriteLine(" {0} | {1} | {2} ", board[6], board[7], board[8]);
         Console.WriteLine();
     }
