281bc4c7588929a19e83d9221db299865f2a97aa
jonat <jonat@JONS-SURFACE>
2025-01-29 14:26:27 -0700

    update to code

--- Mission_4/TicTacTools.cs
+++ Mission_4/TicTacTools.cs
@@ -3,7 +3,7 @@ using System;
 public class TicTacTools
 {
     // Method to print the board
-    public void PrintBoard(string[] board)
+    public static void PrintBoard(string[] board)
     {
         Console.WriteLine("Tic-Tac-Toe Game");
         Console.WriteLine();
